e3ace34b5b7c4f1ddeb307a7f006b8978d0811cf
aussielee <50060992+aussielee@users.noreply.github.com>
2024-10-19 14:39:51 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+# This program calculates your age in the year 2050.
+# Input:  None
+# Output: Your current age followed by your age in 2050
+
+# Create your variables here
+myCurrentAge = 21  # Assign your current age here
+currentYear = 2024  # Assign the current year here
+
+myNewAge = myCurrentAge + (2050 - currentYear)
+print("My Current Age is " + str(myCurrentAge))
+print("I will be " + str(myNewAge) + " in 2050.")
